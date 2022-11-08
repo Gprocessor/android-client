66d239066e4a81b6d6b3abbed37b22f0e1d20e64
Rahul Gill <76812499+rahul-gill@users.noreply.github.com>
2022-11-08 12:41:53 +0530

    Create user.kt

---
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/login/user.kt
@@ -0,0 +1,25 @@
+class ProvideUser{
+   val userStr = """
+    {
+  "username": "mifos",
+  "userId": 1,
+  "base64EncodedAuthenticationKey": "bWlmb3M6cGFzc3dvcmQ=",
+  "authenticated": true,
+  "officeId": 1,
+  "officeName": "Tristate Systems Ltd",
+  "roles": [
+    {
+      "id": 1,
+      "name": "Super user",
+      "description": "This role provides all application permissions.",
+      "disabled": false
+    }
+  ],
+  "permissions": [
+    "ALL_FUNCTIONS"
+  ],
+  "shouldRenewPassword": false,
+  "isTwoFactorAuthenticationRequired": false
+}
+   """
+}
